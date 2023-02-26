6cc8fdebc0094d616fad76300d6a87e8219c65bc
Jackson Ming Hu <huming2207@gmail.com>
2023-02-26 23:24:07 +1100

    main: implement logic

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@
 
 use core::mem::MaybeUninit;
 
+use cortex_m::interrupt::CriticalSection;
 use flash_algorithm::*;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32wlxx_hal::{pac::Peripherals, rcc, flash::{Flash, Page, AlignedAddr}};
 
-struct Algorithm;
+struct Algorithm {
+    dp: Peripherals
+}
 
 algorithm!(Algorithm, {
     flash_address: 0x08000000,
@@ -15,7 +19,7 @@ algorithm!(Algorithm, {
     empty_value: 0xFF,
     sectors: [{
         size: 0x800,
-        address: 0x08000000,
+        address: 0,
     }]
 });
 
@@ -23,26 +27,67 @@ impl FlashAlgorithm for Algorithm {
     fn new(_address: u32, _clock: u32, _function: Function) -> Result<Self, ErrorCode> {
         rtt_init_print!();
         rprintln!("Init");
-        // TODO: Add setup code for the flash algorithm.
-        Ok(Self)
+
+        let mut dp = Peripherals::take().unwrap();
+        let cs = unsafe { &CriticalSection::new() };
+        unsafe {
+            rcc::set_sysclk_msi(
+                &mut dp.FLASH,
+                &mut dp.PWR,
+                &mut dp.RCC,
+                rcc::MsiRange::Range16M, // Maybe we should consider 16MHz here so that no flash wait cycle needed??
+                cs,
+            );
+        }
+
+        Ok(Algorithm { dp })
     }
 
     fn erase_all(&mut self) -> Result<(), ErrorCode> {
         rprintln!("Erase All");
-        // TODO: Add code here that erases the entire flash.
-        Err(ErrorCode::new(0x70d0).unwrap())
+
+        let mut flash = Flash::unlock(&mut self.dp.FLASH);
+        let ret = unsafe { flash.mass_erase() };
+        match ret {
+            Ok(_) => return Ok(()),
+            Err(err) => return Err(ErrorCode::new(0x70d0).unwrap())
+        }
     }
 
     fn erase_sector(&mut self, addr: u32) -> Result<(), ErrorCode> {
-        rprintln!("Erase sector addr:{}", addr);
-        // TODO: Add code here that erases a page to flash.
-        Ok(())
+        rprintln!("Erase sector addr:0x{:04x}", addr);
+
+        let mut flash = Flash::unlock(&mut self.dp.FLASH);
+        let page = match Page::from_addr(addr as usize) {
+            Some(page) => page,
+            None => {
+                return Err(ErrorCode::new(1).unwrap());
+            }
+        };
+
+        let ret = unsafe { flash.page_erase(page) };
+        match ret {
+            Ok(_) => return Ok(()),
+            Err(err) => {
+                return Err(ErrorCode::new(2).unwrap())
+            }
+        }
     }
 
     fn program_page(&mut self, addr: u32, data: &[u8]) -> Result<(), ErrorCode> {
         rprintln!("Program Page addr:{} size:{}", addr, data.len());
-        // TODO: Add code here that writes a page to flash.
-        Ok(())
+
+        let mut flash = Flash::unlock(&mut self.dp.FLASH);
+        let addr = match AlignedAddr::try_from(addr) {
+            Ok(addr) => addr,
+            Err(_) => return Err(ErrorCode::new(1).unwrap())
+        };
+
+        let ret = unsafe { flash.program_bytes(data, addr) };
+        match ret {
+            Ok(_) => return Ok(()),
+            Err(_) => return Err(ErrorCode::new(2).unwrap())
+        }
     }
 }
 
